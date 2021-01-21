b76df7bdfccdec5137ccba2133593c04089cf649
Raghav G <raghav.g@razorpay.com>
2021-01-21 17:53:25 +0530

    Added Mention event and cronbot command

--- src/main/java/com/slackbot/cron/SlackApp.java
+++ src/main/java/com/slackbot/cron/SlackApp.java
@@ -1,8 +1,9 @@
 package com.slackbot.cron;
 
 import com.slack.api.bolt.App;
-import com.slack.api.methods.response.chat.ChatPostMessageResponse;
-import com.slack.api.model.event.MessageEvent;
+// import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+// import com.slack.api.model.event.MessageEvent;
+import com.slack.api.model.event.AppMentionEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,26 +14,39 @@ public class SlackApp {
         //logging.level.com.slack.api = DEBUG
         App app  = new App();
 
-        app.command("/hello", (req, ctx) -> {
+        app.command("/cronbot", (req, ctx) -> {
             System.out.println(req);
             return ctx.ack("Hello to you!!");
         });
 
-        app.event(MessageEvent.class, (payload, ctx) -> {
-            System.out.println(payload);
+        // app.event(MessageEvent.class, (payload, ctx) -> {
+        //     System.out.println(payload);
             
-            MessageEvent event = payload.getEvent();
-            ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
+        //     MessageEvent event = payload.getEvent();
+        //     ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
+        //         .channel(event.getChannel())
+        //         .threadTs(event.getTs())
+        //         .text(s));
+
+        //     if (!message.isOk()) {
+        //         ctx.logger.error("chat.postMessage failed: {}", message.getError());
+        //     }
+
+        //     return ctx.ack();
+        // });
+
+        app.event(AppMentionEvent.class, (payload, ctx) -> {
+            System.out.println(payload);
+            AppMentionEvent event = payload.getEvent();
+                ctx.client().chatPostMessage(r -> r
                 .channel(event.getChannel())
                 .threadTs(event.getTs())
                 .text("Hey"));
 
-            if (!message.isOk()) {
-                ctx.logger.error("chat.postMessage failed: {}", message.getError());
-            }
-
             return ctx.ack();
         });
+
+
         return app;
     }
 }
