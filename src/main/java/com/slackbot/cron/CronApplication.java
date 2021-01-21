c57ec69f42381808624aefc7bc398d695e5744a3
Sahiba Matta <sahiba.matta@razorpay.com>
2021-01-21 10:27:23 +0530

    initial skeleton

---
+++ src/main/java/com/slackbot/cron/CronApplication.java
@@ -0,0 +1,16 @@
+package com.slackbot.cron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class CronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slackbot/cron/CronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slackbot.cron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
